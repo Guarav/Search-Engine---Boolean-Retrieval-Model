0257e1f968c07af18b44489476f38700580e4fea
gaurav.popli <gaurav.popli@tradus.com>
2014-06-08 20:09:04 +0530

    adding stopwords list and ignoring creation of indexes in case of tokens lying in stop words list

--- src/solr/Tokeniser.java
+++ src/solr/Tokeniser.java
@@ -1,7 +1,9 @@
 package solr;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.util.*;
 
 /**
  * Created by gaurav on 5/27/14.
@@ -15,6 +17,10 @@ public class Tokeniser {
 
     private static Tokeniser instance = null;
 
+    private static final String stopWordsFilePath = "stoplist.txt";
+
+    private Set<String> stopWordsList = new HashSet<String>();
+
     public static void main(String [] args)
     {
 
@@ -27,6 +33,30 @@ public class Tokeniser {
         return instance;
     }
 
+    public Tokeniser()
+    {
+        //load stop words list
+        loadStopWords();
+    }
+
+    private void loadStopWords()
+    {
+        File file = new File(stopWordsFilePath);
+
+        if(file.exists()){
+
+            try{
+                BufferedReader bufferedReader = new BufferedReader(new FileReader(file));
+                String line = "";
+                while( (line = bufferedReader.readLine()) != null )
+                    stopWordsList.add(line.trim());
+            }catch (Exception e){
+                e.printStackTrace();
+            }
+
+        }
+    }
+
 
     public List<String> getTokens( String data )
     {
@@ -34,8 +64,12 @@ public class Tokeniser {
         String []terms = data.split("\\s+");
         for(String term : terms)
         {
-            System.out.println(term);
-            tokens.add(term);
+            if( !this.stopWordsList.contains(term) )
+            {
+                System.out.println(term);
+                tokens.add(term);
+            }
+
         }
 
         return tokens;
